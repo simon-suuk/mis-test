17aec399817f30fcc2c9f53dcf453ff050691ab9
Naana-sark <naanaakosuatabuasarkodie2014@gmail.com>
2020-11-16 12:58:16 +0000

    added file

---
+++ read.py
@@ -0,0 +1,10 @@
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+filelocation = askopenfilename() # open the dialog GUI
+with open(filelocation, "rb") as f:  # open the file in reading (rb) mode and call it f
+    pdf = pdftotext.PDF(f)  # store a text version of the pdf file f in pdf variable
+string_of_text = ''
+for text in pdf:
+    string_of_text += text
